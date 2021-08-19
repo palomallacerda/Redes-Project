0f734f887b5b40cba1a74d27d997730ec3916185
PalomaLacerda <psls@ic.ufal.br>
2021-08-19 09:58:56 -0300

    Single Thread Socket

---
+++ singleThread/servidor_singleThread.py
@@ -0,0 +1,32 @@
+from socket import * #importando todos os metódos dessa biblioteca
+
+serverport = 12000 ## porta do serviço que será usada
+serverName = ''
+#metodo socket recebe dois parametros o tipo de socket (no caso da internet, serviço que ele ta 
+# operando ou seja TCP)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#informa qual  o endereço ip associado e a porta usada pelo servidor
+serverSocket.bind((serverName,serverport))
+
+#Colocando no modo escuta, deixando-o pronto para aceitar conexões
+serverSocket.listen(1)
+print('This server is ready to receive on %d port'%(serverport))
+
+while 1:
+    # metodo accept cria conecction socket o endereço de memoria do qual a conexão foi estabelecida, ou seja,
+    # qual socket do cliente#
+    # addr é o endereço ip e a porta de destino
+    connectionSocket, addr = serverSocket.accept()
+    #recebe até 1024 caracteres
+    sentence = connectionSocket.recv(1024)
+    sentence = sentence.decode('utf-8')
+    #transforma para maiusculo com o UPPER
+    capitalizedSentence = sentence.upper()
+    print('Client %s has send: %s, changing to: %s' %(addr, sentence, capitalizedSentence))
+    #manda de volta a frase convertida
+    connectionSocket.send(capitalizedSentence.encode('utf-8'))
+    #encerra conexão
+    connectionSocket.close()
+
+
