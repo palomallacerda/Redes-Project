18fa6324f929cc69555991c8a959a0877fb12b97
PalomaLacerda <psls@ic.ufal.br>
2021-08-21 09:57:06 -0300

    blowfish system

--- multiThread/blowfish.py
+++ multiThread/blowfish.py
@@ -1,14 +1,15 @@
-from os import pipe
+#Sistema de encriptografia usando o método Blowfish
+
+from os import access, pipe, system
 from Crypto.Cipher import Blowfish
 from Crypto import Random
 import base64
 
-
 ## Função que encriptografa
 def Encrypt(msg):
     key = Random.new().read(16) #gera uma chave aleatória entre 0 e 16
-    block_size = Blowfish.block_size
-    iv = Random.new().read(Blowfish.block_size)
+    block_size = Blowfish.block_size #gera o bloco de encriptação
+    iv = Random.new().read(Blowfish.block_size) 
     padding = "*"
     p = lambda s: s+(block_size -len(s)% block_size)*padding
     c = Blowfish.new(key,Blowfish.MODE_CBC,iv)
@@ -24,12 +25,36 @@ def Densencrypt(key, Ecp_msg):
     final = Blowfish.new(key,Blowfish.MODE_CBC,iv)
     return final.decrypt(Encrypted_msg).decode('ascii').rstrip('*')
 
-print('Digite sua mensagem')
-msg = input('')
-Final = Encrypt(msg)
-print(Final[0])
-print("Your encrypted message:")
-print(Final[1])
-print("Your message:")
-print(Densencrypt(Final[0], Final[1]))
+def menu():
+    while True:
+        print(" ╔╦╦╦═╦╗╔═╦═╦══╦═╗╔══╦═╗╔═╦╗╔═╦╦╦╦═╦╦══╦╦╗")
+        print(" ║║║║╦╣╚╣╠╣║║║║║╦╣╚╗╔╣║║║╣╣╚╣║║║║║╔╣╠╗╚╣═║")       
+        print(" ╚══╩═╩═╩═╩═╩╩╩╩═╝.╚╝╚═╝╚═╩═╩═╩══╩╝╚╩══╩╩╝")        
+        print('                              your encryptation program\n\nPlease enter your operation')
+        print('1-Encrypt\n3-Exit\n')
+        action = input('')
+        msg = ''
+        Final = ''
+        if action == '1':
+            print('\nPlease enter your message')
+            msg = input('')
+            Final = Encrypt(msg)
+            print("\nYour encrypted message is:")
+            print(Final[1])
+        elif action == '3':
+            break    
+        else:  # Arrumar uma forma de fazer o loop
+            print("Unexpected input reloading systeam.....\n")
+            menu()
 
+        print("\nDo you want to:\n2-Decode\n3-exit\n")
+        action = input('')
+
+        if action == '2':
+            print("\nYour decoded message:")
+            fim = Densencrypt(Final[0], Final[1])
+            print(fim)
+        if action == '3':
+            break
+
+menu()
