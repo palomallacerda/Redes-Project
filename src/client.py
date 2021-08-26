bb43543233a215243c433ed798b5460b3e9c6a77
yrribeiro <ynk9a@outlook.com>
2021-08-26 12:29:37 -0300

    refactoring

--- src/client.py
+++ src/client.py
@@ -1,12 +1,12 @@
 import socket
-import argparse 
+import argparse
 import blowfish
 from rich.console import Console
 import time
-import base64
+
 console = Console()
 
-def menu(action, msg, Final,key):
+def menu(action, user_message, return_message, key):
         console.print(" ╔╦╦╦═╦╗╔═╦═╦══╦═╗╔══╦═╗╔═╦╗╔═╦╦╦╦═╦╦══╦╦╗", style='bold red')
         console.print(" ║║║║╦╣╚╣╠╣║║║║║╦╣╚╗╔╣║║║╣╣╚╣║║║║║╔╣╠╗╚╣═║", style='bold red')
         console.print(" ╚══╩═╩═╩═╩═╩╩╩╩═╝.╚╝╚═╝╚═╩═╩═╩══╩╝╚╩══╩╩╝", style='bold red')
@@ -14,44 +14,44 @@ def menu(action, msg, Final,key):
         console.print('[1] Encrypt\n[2] Decode\n[3] E X I T\n')
         action = input('> ')
         sck.send(action.encode('utf-8'))
-        if(action!=3):       
+        if(action!=3):
             if action == '1':
                 console.print('\nGive me a message', style='bold red')
-                msg = input('>> ')
-                sck.sendall(msg.encode('utf-8')) #manda mensagem para o servidor  
+                user_message = input('>> ')
+                sck.sendall(user_message.encode('utf-8')) #manda mensagem para o servidor
                 console.print('\nGive me a key with minimum 4 caracters', style='bold red')
                 key = input('>> ')
-                sck.sendall(key.encode('utf-8')) #manda mensagem para o servidor  
-                Final = sck.recv(1024) #recebe mensagem encriptada
+                sck.sendall(key.encode('utf-8')) #manda mensagem para o servidor
+                return_message = sck.recv(1024) #recebe mensagem encriptada
                 console.print("\nENCRYPTED MESSAGE", style='bold yellow')
-                print(Final.decode())
+                print(return_message.decode())
                 time.sleep(0.5)
                 console.print("\nPLEASE WAIT\nRESTARTING SYSTEM....", style='bold yellow')
                 time.sleep(2)
             elif action == '2':
-                if Final == '': # vai inserir uma mensagem pra decodificar//AINDA N TA FUNCIONANDO
+                if return_message == '': # vai inserir uma mensagem pra decodificar//AINDA N TA FUNCIONANDO
                     console.print('\nGive me a encrypted message', style='bold red')
-                    if msg == '':
-                        msg = input('>> ')
+                    if user_message == '':
+                        user_message = input('>> ')
                         console.print('\nGive me a key with minimum 6 caracters', style='bold red')
                         key = input('>> ')
-                        Descrypt = blowfish.decrypt_message(key.encode(), msg)  
+                        decrypted_message = blowfish.decrypt_message(key.encode(), user_message)
                         console.print("\nPLEASE WAIT\nRESTARTING SYSTEM....", style='bold yellow')
                         time.sleep(2)
                         console.print("\nDECODED MESSAGE", style='bold yellow')
-                        print(Descrypt)
+                        print(decrypted_message)
                 else: # vai decodificar a mensagem que já foi inserida anteriormente
-                    console.print("\nPLEASE WAIT\nDESCODING MESSAGE....", style='bold yellow')#colocar uns movimentos 
+                    console.print("\nPLEASE WAIT\nDECODING PREVIOUS MESSAGE...", style='bold yellow')#colocar uns movimentos
                     time.sleep(2)
                     console.print("\nDECODED MESSAGE", style='bold yellow')
-                    Descrypt_msg = sck.recv(1024)
-                    print(Descrypt_msg.decode())
-                    time.sleep(1)                                    
-        if action == '3': 
-            console.print("Shutting down System.....", style='bold yellow')
+                    decrypt_user_message = sck.recv(1024)
+                    print(decrypt_user_message.decode())
+                    time.sleep(1)
+        if action == '3':
+            console.print("Shutting down server...", style='bold yellow')
             time.sleep(0.5)
             return True
-        menu(action, msg, Final, key)
+        menu(action, user_message, return_message, key)
 
 #vc pode mudar o host e a porta de entrada
 parser = argparse.ArgumentParser(description="This is a client for multthreads connections")
@@ -65,11 +65,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
         sck.connect((arg.host, arg.port))
     except Exception as e: #caso apareça algum erro
         raise SystemExit(f"We cant connect to {arg.host} on {arg.port} because: {e}")
-    
+
     while True:
         action = ''
-        msg = ''
-        Final = ''
+        user_message = ''
+        return_message = ''
         key = ''
-        shut = menu(action, msg, Final,key)
+        shut = menu(action, user_message, return_message,key)
         if shut == True: break
--- src/multithread_server.py
+++ src/multithread_server.py
@@ -3,7 +3,7 @@ import socket
 import argparse
 import threading
 import base64
-from typing import final 
+from typing import Final
 import blowfish
 
 # blowfish decodificar
@@ -17,46 +17,45 @@ print(f"Running the server on: {args.host} and port: {args.port}")
 sck = socket.socket()
 sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-try: 
+try:
 	sck.bind((args.host, args.port))
 	sck.listen(5)
 except Exception as e:
 	raise SystemExit(f"We could not bind the server on host: {args.host} to port: {args.port}, because: {e}")
-	
 
-def on_new_client(client, connection, msg, key, Final):
+
+def on_new_client(client, connection, client_message, key, encrypted_message):
 	ip = connection[0]
 	port = connection[1]
 	print(f"A new connection was made from IP: {ip}, and port: {port}!\n Waiting for a message....")
-	action = client.recv(1024)
-	action = action.decode('utf-8')
+	action = client.recv(1024).decode('utf-8')
+
 	while True:
-		if action == '1': 
-			msg = client.recv(1024)
-			msg = msg.decode()
+		if action == '1':
+			client_message = client.recv(1024).decode()
 			key = client.recv(1024)
-			print(f"The client message is: {msg}\n and key:{key}\n")
-			Final = blowfish.encrypt_message(msg, key)
-			print(f"We are sending the encrypted message: {Final[1].decode()}")
-			print(f"We are sending the encrypted key: {Final[0]}")
-			client.sendall(Final[1])
+			print(f"The client message is: {client_message}\n and key:{key}\n")
+			encrypted_message = blowfish.encrypt_message(client_message, key)
+			print(f"We are sending the encrypted message: {encrypted_message[1].decode()}")
+			print(f"We are sending the encrypted key: {encrypted_message[0]}")
+			client.sendall(encrypted_message[1])
 		if action == '2': #arrumar uma forma de reconhecer quando for decodificar uma mensagem já inserida ou n
-			print(f"Decoding the client message: {msg}\n with key:{key}\n")
-			Final = blowfish.encrypt_message(msg, key)
-			Final_des = blowfish.decrypt_message(Final[0],Final[1])
-			print(f"Sending decoded message:{Final_des}")
-			client.sendall(Final_des.encode())
+			print(f"Decoding the client message: {client_message}\n with key:{key}\n")
+			encrypted_message = blowfish.encrypt_message(client_message, key)
+			return_message = blowfish.decrypt_message(encrypted_message[0],encrypted_message[1])
+			print(f"Sending decoded message:{return_message}")
+			client.sendall(return_message.encode())
 		if action == '3':
-			return
-		on_new_client(client, connection, msg, key, Final)
-				
+			break
+		on_new_client(client, connection, client_message, key, encrypted_message)
+
 while True:
-	try: 
+	try:
 		client, ip = sck.accept()
-		Final = ''
-		msg =''
+		encrypted_message = ''
+		client_message =''
 		key = ''
-		threading._start_new_thread(on_new_client,(client, ip,msg, key, Final))
+		threading._start_new_thread(on_new_client,(client, ip,client_message, key, encrypted_message))
 	except KeyboardInterrupt:
 		print('We are shutting down the server!')
 		break
